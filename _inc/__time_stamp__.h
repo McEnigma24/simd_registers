b08713cdef1bfed9a7ca5add45cf9405433f5b01
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-02-11 19:25:56 +0100

    quick

--- _inc/__time_stamp__.h
+++ _inc/__time_stamp__.h
@@ -1,6 +1,7 @@
 #include "__preprocessor__.h"
 
-std::chrono::steady_clock::time_point time_stamp_last_time = std::chrono::steady_clock::now();
+std::chrono::steady_clock::time_point time_stamp_last_time =
+    std::chrono::steady_clock::now();
 
 std::string get_current_local_time()
 {
@@ -16,7 +17,8 @@ std::string get_current_local_time()
 std::string calculate_time_difference()
 {
     auto now = std::chrono::steady_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(now - time_stamp_last_time);
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+        now - time_stamp_last_time);
     time_stamp_last_time = now; // Aktualizuj czas ostatniego pomiaru
 
     // Konwersja na jednostki czasu
@@ -30,13 +32,19 @@ std::string calculate_time_difference()
 
     // Formatowanie wyniku
     std::ostringstream oss;
-    if (hours > 0) oss << hours << "h ";
-    if (minutes > 0 || hours > 0) oss << minutes << "m ";
-    if (seconds > 0 || minutes > 0 || hours > 0) oss << seconds << "s ";
+    if (hours > 0)
+        oss << hours << "h ";
+    if (minutes > 0 || hours > 0)
+        oss << minutes << "m ";
+    if (seconds > 0 || minutes > 0 || hours > 0)
+        oss << seconds << "s ";
     oss << milliseconds << "ms";
     return oss.str();
 }
 
-#define time_stamp(x) std::cout << "\nTIME_STAMP: " << x << " " << get_current_local_time() << " (+ " << calculate_time_difference() << ")\n";
+#define time_stamp(x)                                                          \
+    std::cout << "\nTIME_STAMP: " << setw(50) << left << x << right << " "     \
+              << get_current_local_time() << " (+ "                            \
+              << calculate_time_difference() << ")\n";
 #define time_stamp_reset() calculate_time_difference();
-// #define time_stamp(x) 
+// #define time_stamp(x)
--- _src/main.cpp
+++ _src/main.cpp
@@ -3,47 +3,47 @@
 
 #include "hwy/highway.h"
 
-HWY_BEFORE_NAMESPACE();
-namespace hwy
-{
-    namespace HWY_NAMESPACE
-    {
+// HWY_BEFORE_NAMESPACE();
+// namespace hwy
+// {
+//     namespace HWY_NAMESPACE
+//     {
 
-        void AddVectors(const float* HWY_RESTRICT a,
-                        const float* HWY_RESTRICT b, float* HWY_RESTRICT result,
-                        size_t length)
-        {
-            const HWY_FULL(float) d;
-            for (size_t i = 0; i < length; i += Lanes(d))
-            {
-                const auto va = Load(d, a + i);
-                const auto vb = Load(d, b + i);
-                const auto vresult = Add(va, vb);
-                Store(vresult, d, result + i);
-            }
-        }
+//         void AddVectors(const float* HWY_RESTRICT a,
+//                         const float* HWY_RESTRICT b, float* HWY_RESTRICT
+//                         result, size_t length)
+//         {
+//             const HWY_FULL(float) d;
+//             for (size_t i = 0; i < length; i += Lanes(d))
+//             {
+//                 const auto va = Load(d, a + i);
+//                 const auto vb = Load(d, b + i);
+//                 const auto vresult = Add(va, vb);
+//                 Store(vresult, d, result + i);
+//             }
+//         }
 
-    } // namespace HWY_NAMESPACE
-} // namespace hwy
-HWY_AFTER_NAMESPACE();
+//     } // namespace HWY_NAMESPACE
+// } // namespace hwy
+// HWY_AFTER_NAMESPACE();
 
-#if HWY_ONCE
-namespace hwy
-{
-    HWY_EXPORT(AddVectors);
-} // namespace hwy
-#endif
+// #if HWY_ONCE
+// namespace hwy
+// {
+//     HWY_EXPORT(AddVectors);
+// } // namespace hwy
+// #endif
+
+// const size_t length = 1024;
+// float a[length], b[length], result[length];
+// // Inicjalizacja a i b danymi
+// hwy::AddVectors(a, b, result, length);
 
 int main(int argc, char* argv[])
 {
     srand(time(NULL));
     UTILS::clear_terminal();
     time_stamp("It just works");
-
-    const size_t length = 1024;
-    float a[length], b[length], result[length];
-    // Inicjalizacja a i b danymi
-    hwy::AddVectors(a, b, result, length);
 
     time_stamp("DONE");
     return 0;
